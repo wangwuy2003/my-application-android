90c948aec75bf8f4fccfe0954289f4892dbdcfcf
Nguyen Quang Huy <nquanghuy.2003@gmail.com>
2024-10-24 09:45:02 +0700

    update. layout ex12

---
+++ Ex12/app/src/main/java/vn/edu/ex12/MainActivity.java
@@ -0,0 +1,28 @@
+package vn.edu.ex12;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    TextView selection;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
