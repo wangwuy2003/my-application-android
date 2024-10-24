bbf5d5a5b816042ea47ccc203793abfc61693ebf
Nguyen Quang Huy <nquanghuy.2003@gmail.com>
2024-10-24 09:45:17 +0700

    update. code ex12

--- Ex12/app/src/main/java/vn/edu/ex12/MainActivity.java
+++ Ex12/app/src/main/java/vn/edu/ex12/MainActivity.java
@@ -22,7 +22,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        selection = findViewById(R.id.selection);
+        final String arr1[]  = {"Iphone 7", "Iphone 8", "Iphone X", "Iphone 11", "Iphone 12"};
 
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arr1);
+
+        ListView listView = findViewById(R.id.listView);
+        listView.setAdapter(adapter1);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                selection.setText("Vi tri: " + i + ": " + arr1[i]);
+            }
+        });
 
     }
 }
