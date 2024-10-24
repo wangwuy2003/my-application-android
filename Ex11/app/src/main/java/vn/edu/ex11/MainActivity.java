ea20259a32239d183856350c7610e9296c13262c
Nguyen Quang Huy <nquanghuy.2003@gmail.com>
2024-10-24 08:57:25 +0700

    update layout ex11

--- Ex11/app/src/main/java/vn/edu/ex11/MainActivity.java
+++ Ex11/app/src/main/java/vn/edu/ex11/MainActivity.java
@@ -1,6 +1,11 @@
 package vn.edu.ex11;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +14,16 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
+    EditText edt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        btn = findViewById(R.id.btn);
+        edt = findViewById(R.id.edt);
+
+        
     }
 }
